ee6768b81be7e206b6cc91c14bca3daf06ed130b
carlos-paz12 <carlos.silva.1204@proton.me>
2025-12-05 08:02:32 -0300

    Commit inicial

---
+++ js/music-list.js
@@ -0,0 +1,53 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+let allMusic = [
+  {
+    name: "Abertura",
+    artist: "1/4 Antropológico",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "episódio 01",
+    artist: "1/4 Antropológico",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "episódio 02",
+    artist: "1/4 Antropológico",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "episódio 03",
+    artist: "1/4 Antropológico",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "episódio 04",
+    artist: "1/4 Antropológico",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "episódio 05",
+    artist: "1/4 Antropológico",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "episódio 06",
+    artist: "1/4 Antropológico",
+    img: "music-7",
+    src: "music-7",
+  },
+];
